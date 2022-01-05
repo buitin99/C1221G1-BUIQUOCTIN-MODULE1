e45ad36f1583d017d9e45289ea8ed83ce156072d
buitin99 <buiquoctin.job@gmail.com>
2022-01-05 17:17:27 +0700

    nop bai ss7

--- ss7_bien_kieu_du_lieu_va_toan_tu/bai_tap/Bai6UngDungMayTinh.js
+++ ss7_bien_kieu_du_lieu_va_toan_tu/bai_tap/Bai6UngDungMayTinh.js
@@ -1,7 +1,54 @@
-let i1 = document.getElementById(I1);
-let i2 = document.getElementById(I2);
 
-let t1 = parseInt(i1);
-let t2 = parseInt(i2);
 
-    if()
+
+function toanCong() {
+    let i1 = parseInt(document.getElementById('I1').value);
+    let i2 = parseInt(document.getElementById('I2').value);
+    let phepTinh;
+
+
+
+    if(document.getElementById('add')) {
+            phepTinh = i1 + i2;
+        };
+            alert(phepTinh);
+    };
+
+function toanTru(){
+
+    let i1 = parseInt(document.getElementById('I1').value);
+    let i2 = parseInt(document.getElementById('I2').value);
+    let phepTinh;
+        if(document.getElementById('sub')) {
+            phepTinh = i1 - i2;
+        };
+        alert(phepTinh);
+    }
+
+function toanNhan(){
+
+    let i1 = parseInt(document.getElementById('I1').value);
+    let i2 = parseInt(document.getElementById('I2').value);
+    let phepTinh;
+    if(document.getElementById('mul')) {
+        phepTinh = i1 * i2;
+    };
+    alert(phepTinh);
+}
+
+function toanChia(){
+
+    let i1 = parseInt(document.getElementById('I1').value);
+    let i2 = parseInt(document.getElementById('I2').value);
+    let phepTinh;
+    if(document.getElementById('div')) {
+        phepTinh = i1 / i2;
+    };
+    alert(phepTinh);
+}
+
+
+
+let a = 5;
+let b;
+aler(parseInt(a++ + a--));
---
+++ ss7_bien_kieu_du_lieu_va_toan_tu/bai_tap/Bai7UngDungMayTinh.js
@@ -0,0 +1,21 @@
+
+
+
+function tinhToan(phepTinh) {
+    let i1 = parseInt(document.getElementById('I1').value);
+    let i2 = parseInt(document.getElementById('I2').value);
+
+    if (phepTinh == '+') {
+        alert(i1 + i2);
+    } else if (phepTinh == '-') {
+        alert(i1 - i2);
+    } else if (phepTinh == '*') {
+        alert(i1 * i2);
+    } else
+        alert(i1 / i2);
+    };
+
+
+
+
+
